aef7729a9b214c47592a7cc661c095c86005625f
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2022-08-06 21:33:46 +0700

    react-vite-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div className="flex justify-center">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="w-40" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="w-40 animate-spin" alt="React logo" />
+        </a>
+      </div>
+      <div className="text-3xl mt-20">
+        <h1 className="font-extrabold text-cyan-500">Heiiii</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
